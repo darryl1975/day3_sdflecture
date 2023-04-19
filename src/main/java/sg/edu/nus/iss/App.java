0e6f08eefa3179ac563e2fedff5cb5ec3f07dd89
Darryl Ng <darryl1975@hotmail.com>
2023-04-19 12:20:03 +0800

    different ways to read text file

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -55,40 +55,40 @@ public class App
             fileData.createNewFile();
         }
 
-        // String content = "\nI will like to go home early to rest and start coding practice again.";
-        // String content2 = "\nLet's keep on coding coding coding...";
-        // FileWriter fileWriter = new FileWriter(dirPathFileName, true);
-        // fileWriter.write(content);
-        // fileWriter.write("\n");
-        // fileWriter.write(content2);
-        // fileWriter.flush();
-        // fileWriter.close();
+        String content = "\nI will like to go home early to rest and start coding practice again.";
+        String content2 = "\nLet's keep on coding coding coding...";
+        FileWriter fileWriter = new FileWriter(dirPathFileName, true);
+        fileWriter.write(content);
+        fileWriter.write("\n");
+        fileWriter.write(content2);
+        fileWriter.flush();
+        fileWriter.close();
 
-        // // another example - a type of decorator pattern
-        // String content3 = "\nA quick brown fox jumps over the wall.";
-        // FileWriter fw2 = new FileWriter(dirPathFileName, true);
-        // BufferedWriter bw = new BufferedWriter(fw2);
-        // bw.append(content3);
-        // bw.flush();
-        // bw.close();
-        // fw2.close();
+        // another example - a type of decorator pattern
+        String content3 = "\nA quick brown fox jumps over the wall.";
+        FileWriter fw2 = new FileWriter(dirPathFileName, true);
+        BufferedWriter bw = new BufferedWriter(fw2);
+        bw.append(content3);
+        bw.flush();
+        bw.close();
+        fw2.close();
 
-        // // another example
-        // String content4 = "\nIts good to buy a macbook with 32GB for your development.";
-        // FileOutputStream fos = new FileOutputStream(dirPathFileName, true);
-        // byte[] byteContent = content4.getBytes();
-        // fos.write(byteContent);
-        // fos.flush();
-        // fos.close();
+        // another example
+        String content4 = "\nIts good to buy a macbook with 32GB for your development.";
+        FileOutputStream fos = new FileOutputStream(dirPathFileName, true);
+        byte[] byteContent = content4.getBytes();
+        fos.write(byteContent);
+        fos.flush();
+        fos.close();
 
-        // // another example - with decorator pattern
-        // String content5 = "\nI am going to buy Apple AirTag this weekend";
-        // FileOutputStream fos2 = new FileOutputStream(dirPathFileName, true);
-        // DataOutputStream dos2 = new DataOutputStream(fos2);
-        // dos2.writeBytes(content5);
-        // dos2.flush();
-        // dos2.close();
-        // fos2.close();
+        // another example - with decorator pattern
+        String content5 = "\nI am going to buy Apple AirTag this weekend";
+        FileOutputStream fos2 = new FileOutputStream(dirPathFileName, true);
+        DataOutputStream dos2 = new DataOutputStream(fos2);
+        dos2.writeBytes(content5);
+        dos2.flush();
+        dos2.close();
+        fos2.close();
 
         // reading from file
         File file2 = new File(dirPath + File.separator + fileName); 
